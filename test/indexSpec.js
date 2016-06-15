39b89424c245a8a8e2648b2aa53a50c495d59fd8
Andy Savage <andy@savage.hk>
2016-06-15 23:32:38 +0900

    Updated

---
+++ test/indexSpec.js
@@ -0,0 +1,28 @@
+// var mitv = new MITV("192.168.1.154");
+// // mitv.isAlive(function(err, isAlive) {
+// //   if (err) return console.error(err);
+// //   console.log(isAlive);
+// // });
+// // mitv.getState(function(err, isAlive) {
+// //   if (err) return console.error(err);
+// //   console.log(isAlive);
+// // });
+// // mitv.getVolume(function(err, volume) {
+// //   if (err) return console.error(err);
+// //   console.log(volume);
+// // });
+// // mitv.getInstalledApps(999, function(err, apps) {
+// //   if (err) return console.error(err);
+// //   console.log(apps);
+// // });
+// // mitv.startApp("com.xiaomi.mitv.mediaexplorer", function(err) {
+// //   if (err) return console.error(err);
+// // });
+// // mitv.getSystemInfo(function(err, info) {
+// //   if (err) return console.error(err);
+// //   console.log(info);
+// // });
+// mitv.getRemoteUpgradeInfo(function(err, info) {
+//   if (err) return console.error(err);
+//   console.log(info);
+// });
